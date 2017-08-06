5a183073d5d65d8782c1e32e649eb353d7cb958c
Victor Presumido <victor.presumido@gmail.com>
2017-08-06 00:01:13 -0300

    js to mobile navbar

--- js/init.js
+++ js/init.js
@@ -12,6 +12,23 @@
       itemSelector: '.col',
     });
 
+    // Scroll Reveal
+    window.sr = ScrollReveal();
+    sr.reveal('.card');
 
+    // Mobile button colapse
+	function adjustIndex() {
+      $('body').scrollTop(0);
+      $("#sidenav-overlay").addClass("adjust-index");
+    }
+
+	$(".button-collapse").sideNav({
+      menuWidth: 1000, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
+	$('.button-collapse').click(function(){
+      adjustIndex();
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
